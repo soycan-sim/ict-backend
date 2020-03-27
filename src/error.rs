97d69799d900dcdb3a5a2e7e64e61d8703db9069
Szymon Walter <waltersz@protonmail.com>
2020-03-27 20:09:19 +0100

    refactor

---
+++ src/error.rs
@@ -0,0 +1,56 @@
+use actix_web::ResponseError;
+use std::fmt::{self, Display};
+use std::io::Error as IoError;
+use std::num::ParseIntError;
+use tokio_postgres::Error as DbError;
+
+#[derive(Debug)]
+pub enum Error {
+    Db(DbError),
+    Io(IoError),
+    Template(ParseIntError),
+    Cmdline(String),
+    Useradd,
+    CreateDb,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::Db(err) => Display::fmt(err, f),
+            Error::Io(err) => Display::fmt(err, f),
+            Error::Template(err) => write!(f, "template error: {}", err),
+            Error::Cmdline(err) => write!(f, "command line error: {}", err),
+            Error::Useradd => write!(
+                f,
+                "creating the user `circus` failed (`useradd ... circus`)"
+            ),
+            Error::CreateDb => write!(
+                f,
+                "creating the database `circus` failed (`createdb ... circus`)"
+            ),
+        }
+    }
+}
+
+impl From<DbError> for Error {
+    fn from(err: DbError) -> Error {
+        Error::Db(err)
+    }
+}
+
+impl From<IoError> for Error {
+    fn from(err: IoError) -> Error {
+        Error::Io(err)
+    }
+}
+
+impl From<ParseIntError> for Error {
+    fn from(err: ParseIntError) -> Error {
+        Error::Template(err)
+    }
+}
+
+impl ResponseError for Error {}
+
+pub type Result<T> = std::result::Result<T, Error>;
---
+++ src/web.rs
@@ -0,0 +1,80 @@
+use tokio::{fs, task::JoinHandle};
+use tokio_postgres::{self as psql, NoTls};
+use actix_web::{get, web, http, Responder, HttpResponse};
+
+use crate::error::Result;
+use crate::template;
+
+pub struct ServerData {
+    client: psql::Client,
+    _handle: JoinHandle<()>,
+}
+
+impl ServerData {
+    pub async fn new(config: &str, tls: NoTls) -> Result<Self> {
+        let (client, conn) = psql::connect(config, tls).await?;
+        let handle = tokio::spawn(async move {
+            if let Err(e) = conn.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+        Ok(Self { client, _handle: handle })
+    }
+}
+
+#[get("/style/{sheet}.css")]
+pub async fn stylesheet(_data: web::Data<ServerData>, info: web::Path<String>) -> Result<impl Responder> {
+    let path = format!("public/style/{}.css", info);
+    let sheet = fs::read_to_string(path).await?;
+    Ok(HttpResponse::Ok()
+        .header(http::header::CONTENT_TYPE, "text/css")
+        .body(sheet))
+}
+
+#[get("/frontend/{script}.js")]
+pub async fn javascript(_data: web::Data<ServerData>, info: web::Path<String>) -> Result<impl Responder> {
+    let path = format!("public/frontend/{}.js", info);
+    let script = fs::read_to_string(path).await?;
+    Ok(HttpResponse::Ok()
+        .header(http::header::CONTENT_TYPE, "text/javascript")
+        .body(script))
+}
+
+#[get("/frontend/{script}.wasm")]
+pub async fn wasm(_data: web::Data<ServerData>, info: web::Path<String>) -> Result<impl Responder> {
+    let path = format!("public/frontend/{}.wasm", info);
+    let script = fs::read_to_string(path).await?;
+    Ok(HttpResponse::Ok()
+        .header(http::header::CONTENT_TYPE, "application/wasm")
+        .body(script))
+}
+
+#[get("/articles/{article}")]
+pub async fn articles(data: web::Data<ServerData>, info: web::Path<String>) -> Result<impl Responder> {
+    let path = "public/articles/template.html";
+    let mut body = fs::read_to_string(path).await?;
+    template::search_replace(&data.client, &mut body, &[&format!("articles/{}", info)]).await?;
+    Ok(HttpResponse::Ok()
+        .header(http::header::CONTENT_TYPE, "text/html")
+        .body(body))
+}
+
+#[get("/{res}.html")]
+pub async fn index(data: web::Data<ServerData>, info: web::Path<String>) -> Result<impl Responder> {
+    let path = format!("public/{}.html", info);
+    let mut body = fs::read_to_string(path).await?;
+    template::search_replace(&data.client, &mut body, &[]).await?;
+    Ok(HttpResponse::Ok()
+        .header(http::header::CONTENT_TYPE, "text/html")
+        .body(body))
+}
+
+#[get("/")]
+pub async fn root(data: web::Data<ServerData>) -> Result<impl Responder> {
+    let path = "public/index.html";
+    let mut body = fs::read_to_string(path).await?;
+    template::search_replace(&data.client, &mut body, &[]).await?;
+    Ok(HttpResponse::Ok()
+        .header(http::header::CONTENT_TYPE, "text/html")
+        .body(body))
+}
